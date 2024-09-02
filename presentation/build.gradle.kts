dbb0701e105795c3a0d8afeb9920690c1674b882
정영석 <0423jys@naver.com>
2024-09-03 01:57:05 +0900

    version 1.0.8

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         minSdk = 26
         targetSdk = 34
 
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
